fea32d4020a6835bc6966b83a98a738702de4544
emrehan <emrehan.tuzun@gmail.com>
2014-04-16 02:50:17 +0300

    add pipe functionality  - doesn't favorite any tweets from last 100 favorited users anymore

---
+++ pipe.rb
@@ -0,0 +1,69 @@
+# a ruby 'pipe'
+# works like a queue but only accepts unique elements
+# and have a length of 100 items
+# modified based on https://gist.github.com/daddz/352509
+
+class Pipe
+	
+	def initialize(file_name)
+		@file_name = file_name
+	end
+
+	def push(obj)
+		if include?(obj) == false
+			safe_open('a') do |file|
+				file.write(obj + "\n")
+			end
+		end
+
+		if length > 100
+			pop
+		end
+	end
+
+	alias << push
+
+	def include?(obj)
+		content = nil
+		safe_open('r') do |file|
+			content = file.read 
+		end
+		content.include? obj
+	end
+
+	def exclude?(obj)
+		! include?(obj)
+	end
+
+	private
+	def length
+		count = 0
+		safe_open('r') do |file|
+			count = file.read.count("\n")
+		end
+		count
+	end
+
+	private
+	def safe_open(mode)
+		File.open(@file_name, mode) do |file|
+			file.flock(File::LOCK_EX)
+			yield file
+			file.flock(File::LOCK_UN)
+		end
+	end
+
+	private
+	def pop
+		value = nil
+		rest = nil
+		safe_open('r') do |file|
+			value = file.gets
+			rest = file.read
+		end
+		safe_open('w+') do |file|
+			file.write(rest)
+		end
+	end
+
+end
